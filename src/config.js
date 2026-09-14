b79bbcbacf9ba61de73e9ce62509b1137b60c407
sureshmagnolia <118750471+sureshmagnolia@users.noreply.github.com>
2026-09-14 15:58:46 +0530

    Update APPS_SCRIPT_URL in config.js

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
 
 export const CONFIG = {
   // ⚠️ REPLACE THIS with your deployed Google Apps Script Web App URL
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw29XuhvNI4cV-tlAWz5IaRrWPY1T9P7ZiQJbu-7za9226PyEqlhuLOrOMTG2QulzzOog/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwxE2wnrBwIeKAeVgW8jiae-ohPxp66dMcKA8Awyg2FsiP-wJwAHH9Uw426Uqfk-feSQg/exec',
 
   // Election date for age cutoff (YYYY-MM-DD)
   ELECTION_DATE: '2026-10-12',
